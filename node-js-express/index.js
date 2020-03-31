49119c16528a5c2259028ae6c683867c1eea326d
Irina <zaletk.irina@gmail.com>
2020-03-31 12:26:31 +0300

    feat: урок 1: Настройка приложения Урок 2: Работа с HTML-файлами

---
+++ node-js-express/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express(); //аналон объекта сервер
+
+app.get('/', (req, res) =>{
+  res.status(200)
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+
+app.get('/about', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+  console.log(`server is runnong on port ${PORT}`)
+})
